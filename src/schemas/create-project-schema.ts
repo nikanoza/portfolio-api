5048c71fd5ff0a9ae38e0d6cbf1249ed8de67567
hilu <nikanozadze1010@gmail.com>
2023-01-24 13:45:04 +0400

    feat: create joi validation schema

---
+++ src/schemas/create-project-schema.ts
@@ -0,0 +1,45 @@
+import Joi from "joi";
+import Project from "models/Project";
+import { NewProject, ProjectType } from "types";
+
+const determineIfProjectExists =
+  (project: ProjectType | null) => (value: string, helpers: any) => {
+    if (project) {
+      return helpers.messages("there is project with this name");
+    }
+
+    return value;
+  };
+
+const createProjectSchema = async (data: NewProject) => {
+  const project = await Project.findOne({ name: data.name });
+
+  return Joi.object<NewProject>({
+    name: Joi.string()
+      .required()
+      .custom(determineIfProjectExists(project))
+      .messages({
+        "string.base": "name must be a string",
+        "any.required": "name is required",
+      }),
+    description: Joi.string().required().messages({
+      "string.base": "description must be a string",
+      "any.required": "description is required",
+    }),
+    url: Joi.string().required().messages({
+      "string.base": "url must be a string",
+      "any.required": "url is required",
+    }),
+    tag: Joi.string().required().messages({
+      "string.base": "tag must be a string",
+      "any.required": "tag is required",
+    }),
+    tools: Joi.array().items(
+      Joi.string().messages({
+        "string.base": "tool name must be a string",
+      })
+    ),
+  });
+};
+
+export default createProjectSchema;
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,11 @@
-export interface ProjectType {
+export interface NewProject {
   name: string;
   description: string;
   url: string;
   tag: string;
   tools: string[];
+}
+
+export interface ProjectType extends NewProject {
   id: string;
 }
