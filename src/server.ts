178666e8257cc040b2c4dc99601f7a179d3d61f8
hilu <nikanozadze1010@gmail.com>
2023-01-24 18:53:31 +0400

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,14 @@ import dotenv from "dotenv";
 import connect from "./config/mongo.js";
 import projectRouter from "./routes/project-router.js";
 import swaggerMiddleware from "./swagger-middleware.js";
+import cors from "cors";
 
 dotenv.config();
 connect();
 
 const server = express();
 
-server.use("/api", projectRouter);
+server.use("/api", cors(), projectRouter);
 server.use("/", ...swaggerMiddleware);
 
 server.listen(process.env.PORT || 3000);
