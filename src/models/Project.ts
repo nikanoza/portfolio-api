a3d074209977d66612ac3b687d8e615a0342ed24
hilu <nikanozadze1010@gmail.com>
2023-01-24 12:55:21 +0400

    feat: create project model

---
+++ src/models/Project.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+import { ProjectType } from "../types";
+
+const projectSchema = new Schema<ProjectType>({
+  name: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  description: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  url: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  tag: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  tools: {
+    type: Schema.Types.Mixed,
+    required: true,
+  },
+  id: {
+    type: Schema.Types.String,
+    required: true,
+  },
+});
+
+const Project = model("Project", projectSchema);
+
+export default Project;
---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+export interface ProjectType {
+  name: string;
+  description: string;
+  url: string;
+  tag: string;
+  tools: string[];
+  id: string;
+}
