e041ba45556aae7416f5c2076fd3d41481bffd24
hilu <nikanozadze1010@gmail.com>
2023-01-25 22:09:06 +0400

    feat: add body-parser and cors

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,15 @@ import connect from "./config/mongo.js";
 import projectRouter from "./routes/project-router.js";
 import swaggerMiddleware from "./swagger-middleware.js";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 dotenv.config();
 connect();
 
 const server = express();
+
+server.use(bodyParser.json());
+server.use(cors());
 
 server.use("/storage", express.static("public/storage"));
 server.use("/api", cors(), projectRouter);
